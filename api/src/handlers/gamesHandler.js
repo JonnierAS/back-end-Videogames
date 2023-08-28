8d8f227a76433567ff646e6de564f7505a7bfc20
Jonnier <jonniersalaverria@gmail.com>
2023-08-28 18:50:39 -0500

    Configuracion de handlers

--- api/src/handlers/gamesHandler.js
+++ api/src/handlers/gamesHandler.js
@@ -1,24 +1,54 @@
-
-//Hara 2 cosas nos traera los games o Los games por Name
+//Se encargara de recibir la request, unificar datos
+//Devolver la respuesta
+//Invocar al controller, la cual es otra funcion con logica
+//------------------------------------------------------------//
+const { postGames } = require("../controllers/postGames");
+const {getGamesById} = require("../controllers/getGamesById");
+const {getAllGames} = require("../controllers/getAllGames");
+const {getGamesByName} = require("../controllers/getGamesByName");
+//getGamesHandler Hara 2 cosas nos traera todos los games o Los games por Name
 // a travez de /videogames/?name="Fortnite"
-const getGamesHandler = (req, res)=>{//query
-    const {name} = req.query;
-    if(name) res.status(200).send(`Aqui esta el game ${name}`)
-    res.json("Todos los games")
+const getGamesHandler = async(req, res) => {//query
+    const { name } = req.query;
+    if (name) {
+      try {
+      const data = await getGamesByName(name);
+      res.send(data);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+    } else {
+      try {
+        const data = await getAllGames();
+        res.send(data);
+      } catch (error) {
+        res.status(500).send(error)
+      }
+    }
 };
 
 
 
-
-const getGamesByIdHandler = (req, res)=>{//params
-
-    res.json("Games por Id")
+const getGamesByIdHandler = async(req, res) => {//params
+    const {id} = req.params;
+    try {
+        res.send(await getGamesById(id))
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+    
 };
 
 
-const createGamesHandler = (req, res)=>{//body
-    const {name, date} = req.body;
-    res.status(200).json(`Game ${name} lanzado el ${date}`)
+const createGamesHandler = async (req, res) => {//body
+    const { name, description, platforms, background_image, released, rating, genres } = req.body;
+
+    try {
+        const response = await postGames(name, description, platforms, background_image, released, rating, genres)
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
 };
 
 module.exports = {
--- api/src/handlers/genresHandler.js
+++ api/src/handlers/genresHandler.js
@@ -1,5 +1,12 @@
-const getGenresHandler = (req, res)=>{
-    res.json("Genres")
+const {getGenres} = require("../controllers/getGenres")
+
+const getGenresHandler = async(req, res)=>{
+    try {
+        res.send(await getGenres());
+      } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+      }
 };
 
 
