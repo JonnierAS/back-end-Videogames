1cb639e86ea203219518fcf37cf6fc6650560777
Jonnier <jonniersalaverria@gmail.com>
2023-08-29 14:37:34 -0500

    Solicitud con las props indicadas

--- api/src/controllers/getGamesById.js
+++ api/src/controllers/getGamesById.js
@@ -4,18 +4,30 @@ const axios = require("axios");
 const {Videogame, Genres} = require("../db.js");
 
 const getGamesById = async(id)=>{
-   const numericId = !isNaN(+id)
-  if (numericId) {
+   const numericId = !isNaN(+id)//verificamos si es number o no
+  if (numericId) {//si es verdadero hace la peticion
     const fetchedGame = await axios.get(`https://api.rawg.io/api/games/${id}?key=${API_KEY}`, {
       headers: {
         "Accept-Encoding": "null",
       },    
     });
-    return fetchedGame.data;
+    const fetchedGameArray = [fetchedGame.data];//guardo el juego obtenido en la variable
+    const modifiedGame = fetchedGameArray.map(game => ({//devuelve un nuevo objeto con las props indicadas
+      name: game.name,
+      description: game.description,
+      platforms: game.platforms,
+      background_image: game.background_image,
+      released: game.released,
+      rating: game.rating,
+      genres: game.genres
+  }))
+  return modifiedGame;
   }
+
+  //si numericId es false buscara el id en la base de datos en formato "e118a594-303c-401e-b965-0fd490f55203"
   const detail = await Videogame.findOne({
     where: { id },
-    include: Genres
+    include: Genres //debe incluir los generos asociados en la consulta
   });
   if (!detail) throw { message: "Not Found!" };
   return detail;
