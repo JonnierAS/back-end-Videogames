f0f74d41d2472cbc48ad0b695d7a781a1f56d7c8
Jonnier <jonniersalaverria@gmail.com>
2023-09-13 18:59:57 -0500

    dialectOptions

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT , DB_NAME,/* POSTGRE_URL */
 } = process.env;
-const POSTGRE_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+// const POSTGRE_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 const sequelize = new Sequelize(POSTGRE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
